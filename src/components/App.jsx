d80dcf12ce23ffd182e9217e8373bb6b3c50241f
NataliiaPasaman <natapasaman@gmail.com>
2022-11-17 23:30:16 +0200

    upddate package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,7 @@
-import { useState } from 'react';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { Statistics } from './Statistics/Statistics';
-import { Notification } from 'components/Notification/Notification';
-import { Section } from './Section/Section';
+
 
 export const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
 
-  const onLeaveFeedback = evt => {
-    const { name } = evt.currentTarget;
-
-    switch (name) {
-      case 'good':
-        setGood(good + 1);
-        break;
-      case 'neutral':
-        setNeutral(neutral + 1);
-        break;
-      case 'bad':
-        setBad(bad + 1);
-        break;
-
-      default:
-        return;
-    }
-  };
-
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  const countPositiveFeedbackPercentage = () => {
-    const total = countTotalFeedback();
-    const positivePercentage = Number(((good * 100) / total).toFixed());
-    return positivePercentage;
-  };
-
-  const options={ good, neutral, bad, };
     return (
       <div
         style={{
@@ -47,24 +10,6 @@ export const App = () => {
           color: '#010101',
         }}
       >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={onLeaveFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {countTotalFeedback() !== 0 ? (
-            <Statistics
-              options={options}
-              totalFeedback={countTotalFeedback}
-              positivePercentage={countPositiveFeedbackPercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
       </div>
     );
 
